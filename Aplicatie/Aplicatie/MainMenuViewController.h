35770ce8bf46936e2ba5e6d6e0fa2c4a1ca8aa96
Vochescu Alexandru <valex@valexandru.com>
2014-08-07 12:27:02 +0300

    Created the main menu

---
+++ Aplicatie/Aplicatie/MainMenuViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainMenuViewController.h
+//  Aplicatie
+//
+//  Created by Alexandru Vochescu on 07/08/14.
+//  Copyright (c) 2014 Alexandru Vochescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+<UITableViewDataSource, UITableViewDelegate>
+
+@property IBOutlet UITableView *courses;
+@property NSMutableArray *coursesArray;
+@property int numberOfCourses;
+@end
---
+++ Aplicatie/Aplicatie/MainMenuViewController.m
@@ -0,0 +1,77 @@
+//
+//  MainMenuViewController.m
+//  Aplicatie
+//
+//  Created by Alexandru Vochescu on 07/08/14.
+//  Copyright (c) 2014 Alexandru Vochescu. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    self.numberOfCourses=2;
+    if (self.coursesArray==nil)
+    {
+        self.coursesArray = [[NSMutableArray alloc] init];
+    }
+    [self.coursesArray addObject: @"Fizica"];
+    [self.coursesArray addObject: @"Istorie"];
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.numberOfCourses;
+}
+
+-(UITableViewCell *) tableView: (UITableView *) tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Ma intreaba de linia: %d", indexPath.row);
+    static NSString *identifier = @"Nimic concret";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: identifier];
+    if (cell==nil)
+    {
+        NSLog(@"Se creeaza un nou cell");
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier: identifier];
+    }
+    cell.imageView.image = [UIImage imageNamed: [NSString stringWithFormat:@"%@.jpg", [self.coursesArray objectAtIndex:indexPath.row]]];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.coursesArray objectAtIndex:indexPath.row]];
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
