af7e4e5b15e13dd20229ef66f14e4d25ee0154b1
valexandru <valex@valexandru.com>
2014-08-08 12:31:13 +0300

    Created the Chapter view layout

---
+++ Aplicatie/Aplicatie/ChapterViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChapterViewController.h
+//  Aplicatie
+//
+//  Created by Alexandru Vochescu on 08/08/14.
+//  Copyright (c) 2014 Alexandru Vochescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChapterViewController : UIViewController
+
+@property IBOutlet UILabel* titleForLayout;
+@property IBOutlet UITableView* lessonsList;
+
+@end
