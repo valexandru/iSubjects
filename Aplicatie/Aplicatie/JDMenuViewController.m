fa1c6f3a98419cce8daadf34ecdad2d66257189d
valexandru <valex@valexandru.com>
2014-08-13 11:46:10 +0300

    Last version

--- Aplicatie/Aplicatie/JDMenuViewController.m
+++ Aplicatie/Aplicatie/JDMenuViewController.m
@@ -7,32 +7,41 @@
 //
 
 #import "UIViewController+JDSideMenu.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 #import "JDMenuViewController.h"
 
-@interface JDMenuViewController ()
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-- (IBAction)switchController:(id)sender;
-@end
+//@interface JDMenuViewController ()
+//@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+//- (IBAction)switchController:(id)sender;
+//@end
 
 @implementation JDMenuViewController
 
-- (void)viewDidLayoutSubviews;
+- (void)viewDidLoad
 {
-    [super viewDidLayoutSubviews];
-    self.scrollView.contentSize = CGRectInset(self.scrollView.bounds, 0, -1).size;
+    [super viewDidLoad];
+    [FBSettings setDefaultAppID: @"1436591166630311"];
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    [self.view addSubview:loginView];
+    // Do any additional setup after loading the view.
 }
-
-- (IBAction)switchController:(id)sender;
-{
-    CGFloat randomHue = (arc4random()%256/256.0);
-    UIViewController *viewController = [[UIViewController alloc] init];
-    viewController.view.backgroundColor = [UIColor colorWithHue:randomHue saturation:1.0 brightness:1.0 alpha:1.0];
-    viewController.title = [NSString stringWithFormat: @"Hue: %.2f", randomHue];
-    
-    UIViewController *contentController = [[UINavigationController alloc]
-                                           initWithRootViewController:viewController];
-    [self.sideMenuController setContentController:contentController animated:YES];
-}
+//- (void)viewDidLayoutSubviews;
+//{
+//    [super viewDidLayoutSubviews];
+//    self.scrollView.contentSize = CGRectInset(self.scrollView.bounds, 0, -1).size;
+//}
+//
+//- (IBAction)switchController:(id)sender;
+//{
+//    CGFloat randomHue = (arc4random()%256/256.0);
+//    UIViewController *viewController = [[UIViewController alloc] init];
+//    viewController.view.backgroundColor = [UIColor colorWithHue:randomHue saturation:1.0 brightness:1.0 alpha:1.0];
+//    viewController.title = [NSString stringWithFormat: @"Hue: %.2f", randomHue];
+//    
+//    UIViewController *contentController = [[UINavigationController alloc]
+//                                           initWithRootViewController:viewController];
+//    [self.sideMenuController setContentController:contentController animated:YES];
+//}
 
 @end
